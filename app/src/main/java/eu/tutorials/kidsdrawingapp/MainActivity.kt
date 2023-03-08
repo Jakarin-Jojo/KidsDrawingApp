3824f77ff3f57ab8124c68744e1ef0a2dc265e86
Jakarin-Jojo <jakarin.chujan@gmail.com>
2023-03-09 04:31:49 +0700

    :sparkles:Adding a Share Feature:sparkles:

--- app/src/main/java/eu/tutorials/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/kidsdrawingapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -205,6 +206,7 @@ class MainActivity : AppCompatActivity() {
                             Toast.makeText(this@MainActivity,
                             "File saved Successfully : $result",
                             Toast.LENGTH_SHORT).show()
+                            shareImage(result)
                         }else{
                             Toast.makeText(this@MainActivity,
                             "Something wrong while saving the file.",
@@ -244,6 +246,18 @@ class MainActivity : AppCompatActivity() {
         if(customProgressDialog != null){
             customProgressDialog?.dismiss()
             customProgressDialog = null
+        }
+    }
+
+    private fun shareImage(result: String){
+        MediaScannerConnection.scanFile(this, arrayOf(result), null){
+            path, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
+
         }
     }
 
